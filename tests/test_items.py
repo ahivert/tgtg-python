2be3469e0d42cba1da80a2d177701c84560fb99a
Anthony Hivert <anthony.hivert@gmail.com>
2019-10-01 19:55:35 +0200

    Added get_all_business method (#2)
    - Modified client to authorize client init without credentials

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,8 +1,10 @@
 from urllib.parse import urljoin
 
+import pytest
 import responses
 
 from tgtg import API_ITEM_ENDPOINT, BASE_URL, TgtgClient
+from tgtg.exceptions import TgtgAPIError
 
 
 @responses.activate
@@ -15,3 +17,16 @@ def test_get_items_success():
     )
     client = TgtgClient(access_token="an_access_token", user_id=1234)
     assert client.get_items() == []
+
+
+@responses.activate
+def test_get_items_fail():
+    responses.add(
+        responses.POST,
+        urljoin(BASE_URL, API_ITEM_ENDPOINT),
+        json={"status": 401, "error": "Unauthorized"},
+        status=200,
+    )
+    client = TgtgClient(access_token="an_access_token", user_id=1234)
+    with pytest.raises(TgtgAPIError):
+        client.get_items()
--- tests/test_login.py
+++ tests/test_login.py
@@ -8,7 +8,7 @@ from tgtg.exceptions import TgtgLoginError
 
 
 @responses.activate
-def test_init_client_with_email_password_success():
+def test_login_with_email_password_success():
     responses.add(
         responses.POST,
         urljoin(BASE_URL, LOGIN_ENDPOINT),
@@ -21,12 +21,13 @@ def test_init_client_with_email_password_success():
         status=200,
     )
     client = TgtgClient(email="test@test.com", password="test")
+    client._login()
     assert client.access_token == "an_access_token"
     assert client.user_id == 1234
 
 
 @responses.activate
-def test_init_client_with_bad_email_or_password_fail():
+def test_login_with_bad_email_or_password_fail():
     responses.add(
         responses.POST,
         urljoin(BASE_URL, LOGIN_ENDPOINT),
@@ -37,25 +38,26 @@ def test_init_client_with_bad_email_or_password_fail():
         status=200,
     )
     with pytest.raises(TgtgLoginError):
-        TgtgClient(email="test@test.com", password="test")
+        TgtgClient(email="test@test.com", password="test")._login()
 
 
-def test_init_client_with_token_user_id_success():
+def test_login_with_token_user_id_success():
     client = TgtgClient(access_token="an_access_token", user_id=1234)
+    client._login()
     assert client.access_token == "an_access_token"
     assert client.user_id == 1234
 
 
-def test_init_client_empty_fail():
+def test_login_empty_fail():
     with pytest.raises(ValueError):
-        TgtgClient()
+        TgtgClient()._login()
 
 
-def test_init_client_empty_email_fail():
+def test_login_empty_email_fail():
     with pytest.raises(ValueError):
-        TgtgClient(password="test")
+        TgtgClient(password="test")._login()
 
 
-def test_init_client_empty_token_fail():
+def test_login_empty_token_fail():
     with pytest.raises(ValueError):
-        TgtgClient(user_id=1234)
+        TgtgClient(user_id=1234)._login()
