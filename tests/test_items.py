72d006ffbbea9f792f90acbaad6db6e87b14c399
Anthony Hivert <ahivert@MacBook-Air-de-Anthony.local>
2021-11-23 22:29:01 +0100

    Cleanup + Fix tests
    - fix token refresh
    - cleanup
    - fix readme

--- tests/test_items.py
+++ tests/test_items.py
@@ -6,15 +6,17 @@ import responses
 from tgtg import API_ITEM_ENDPOINT, BASE_URL, TgtgClient
 from tgtg.exceptions import TgtgAPIError
 
+from .constants import tgtg_client_fake_tokens
 
-def test_get_items_success(login_response):
+
+def test_get_items_success(refresh_tokens_response):
     responses.add(
         responses.POST,
         urljoin(BASE_URL, API_ITEM_ENDPOINT),
         json={"items": []},
         status=200,
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     assert client.get_items() == []
     assert (
         len([call for call in responses.calls if API_ITEM_ENDPOINT in call.request.url])
@@ -22,7 +24,7 @@ def test_get_items_success(login_response):
     )
 
 
-def test_get_items_custom_user_agent(login_response):
+def test_get_items_custom_user_agent(refresh_tokens_response):
     responses.add(
         responses.POST,
         urljoin(BASE_URL, API_ITEM_ENDPOINT),
@@ -30,9 +32,7 @@ def test_get_items_custom_user_agent(login_response):
         status=200,
     )
     custom_user_agent = "test"
-    client = TgtgClient(
-        email="test@test.com", password="test", user_agent=custom_user_agent
-    )
+    client = TgtgClient(**tgtg_client_fake_tokens, user_agent=custom_user_agent)
     client.get_items()
     assert (
         len([call for call in responses.calls if API_ITEM_ENDPOINT in call.request.url])
@@ -41,20 +41,20 @@ def test_get_items_custom_user_agent(login_response):
     assert responses.calls[0].request.headers["user-agent"] == custom_user_agent
 
 
-def test_get_items_fail(login_response):
+def test_get_items_fail(refresh_tokens_response):
     responses.add(
         responses.POST, urljoin(BASE_URL, API_ITEM_ENDPOINT), json={}, status=400
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     with pytest.raises(TgtgAPIError):
         client.get_items()
 
 
-def test_get_item_success(login_response):
+def test_get_item_success(refresh_tokens_response):
     responses.add(
         responses.POST, urljoin(BASE_URL, API_ITEM_ENDPOINT) + "1", json={}, status=200
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     assert client.get_item(1) == {}
     assert (
         len([call for call in responses.calls if API_ITEM_ENDPOINT in call.request.url])
@@ -62,23 +62,23 @@ def test_get_item_success(login_response):
     )
 
 
-def test_get_item_fail(login_response):
+def test_get_item_fail(refresh_tokens_response):
     responses.add(
         responses.POST, urljoin(BASE_URL, API_ITEM_ENDPOINT) + "1", json={}, status=400
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     with pytest.raises(TgtgAPIError):
         client.get_item(1)
 
 
-def test_set_favorite(login_response):
+def test_set_favorite(refresh_tokens_response):
     responses.add(
         responses.POST,
         urljoin(BASE_URL, API_ITEM_ENDPOINT) + "1/setFavorite",
         json={},
         status=200,
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     assert client.set_favorite(1, True) is None
     assert (
         len([call for call in responses.calls if API_ITEM_ENDPOINT in call.request.url])
@@ -86,13 +86,13 @@ def test_set_favorite(login_response):
     )
 
 
-def test_set_favorite_fail(login_response):
+def test_set_favorite_fail(refresh_tokens_response):
     responses.add(
         responses.POST,
         urljoin(BASE_URL, API_ITEM_ENDPOINT) + "1/setFavorite",
         json={},
         status=400,
     )
-    client = TgtgClient(email="test@test.com", password="test")
+    client = TgtgClient(**tgtg_client_fake_tokens)
     with pytest.raises(TgtgAPIError):
         client.set_favorite(1, True)
