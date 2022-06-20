fa2216e925a381da92cf37ff8ad2771805e7499d
Anthony Hivert <anthony.hivert@gmail.com>
2022-06-20 20:32:53 +0200

    Build user agent from last APK version available in playstore

---
+++ tgtg/google_play_scraper.py
@@ -0,0 +1,17 @@
+import json
+import re
+
+import requests
+
+RE_SCRIPT = re.compile(
+    r"AF_initDataCallback\({key:\s*'ds:4'.*?data:([\s\S]*?), sideChannel:.+<\/script"
+)
+
+
+def get_last_apk_version():
+    response = requests.get(
+        "https://play.google.com/store/apps/details?id=com.app.tgtg&hl=en&gl=US"
+    )
+    match = RE_SCRIPT.search(response.text)
+    data = json.loads(match.group(1))
+    return data[1][2][140][0][0][0]
